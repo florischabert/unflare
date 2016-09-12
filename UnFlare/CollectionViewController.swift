f7ea0989786b3b322fb95618bae9392bb271292e
Floris Chabert <chabertf@gmail.com>
2016-09-11 21:09:52 -0700

    Main app flow complete.

--- UnFlare/CollectionViewController.swift
+++ UnFlare/CollectionViewController.swift
@@ -9,32 +9,52 @@
 import UIKit
 import Photos
 
-// TODO: What if library access not allowed?
-// TODO: Image thumbnail size?
-// TODO: Landscape mode?
+class ViewCell : UICollectionViewCell {
+    var asset: PHAsset?
+}
 
 class CollectionViewController: UICollectionViewController, UICollectionViewDelegateFlowLayout {
 
     var assets: PHFetchResult<PHAsset>?
-    var firstTime = true
+    var status: PHAuthorizationStatus?
         
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        let options = PHFetchOptions()
-        options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
-        assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: options)
     }
     
     override func viewDidAppear(_ animated: Bool) {
-        if firstTime {
-            collectionView?.scrollRectToVisible(CGRect(origin: CGPoint(x: collectionView!.contentSize.width-1, y: collectionView!.contentSize.height-1), size: CGSize(width: 1, height: 1)), animated: false)
-            firstTime = false
+        PHPhotoLibrary.requestAuthorization { status in
+            if self.status != status {
+                self.status = status
+                self.updateUI()
+            }
+        }
+    }
+    
+    func updateUI() {
+        if status == .denied {
+                DispatchQueue.main.async {
+                    self.performSegue(withIdentifier: "Denied", sender: self)
+                }
+            }
+        else {
+            DispatchQueue.global().async {
+                let options = PHFetchOptions()
+                options.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: true)]
+                self.assets = PHAsset.fetchAssets(with: PHAssetMediaType.image, options: options)
+                DispatchQueue.main.async {
+                    self.collectionView?.reloadData()
+                    
+                    let lastItem = self.collectionView?.numberOfItems(inSection: 0)
+                    let indexPath = IndexPath(item: lastItem!-1, section: 0)
+                    self.collectionView?.scrollToItem(at: indexPath, at: .bottom, animated: false)
+                }
+            }
         }
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 1
+        return 0
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
@@ -46,39 +66,49 @@ class CollectionViewController: UICollectionViewController, UICollectionViewDele
     }
     
     func collectionView(_ collection: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let size = view.bounds.size.width/4-1
+        var size = view.bounds.size.width/4
+        if indexPath.row % 4 != 3  {
+            size -= 1
+        }
         return CGSize(width: size, height: size)
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ViewCell
         let imageView = cell.viewWithTag(42) as! UIImageView
         let asset = assets!.object(at: indexPath.row)
+        cell.asset = asset
 
-        let options = PHImageRequestOptions()
-        options.resizeMode = .exact
-        
-        let cropSideLength = min(asset.pixelWidth, asset.pixelHeight)
-        let square = CGRect(x: 0, y: 0, width: cropSideLength, height: cropSideLength)
-        let cropRect = square.applying(CGAffineTransform(scaleX: 1.0 / CGFloat(asset.pixelWidth), y: 1.0 / CGFloat(asset.pixelHeight)))
-        options.normalizedCropRect = cropRect
-        
-        PHImageManager.default().requestImage(for: asset, targetSize: CGSize(width: asset.pixelWidth/10, height: asset.pixelHeight/10), contentMode: .aspectFit, options: options, resultHandler: {(result, info)->Void in
-            DispatchQueue.main.async {
-                imageView.image = result
-            }
-        })
+//        DispatchQueue.global().async {
+            let options = PHImageRequestOptions()
+            options.resizeMode = .exact
+            options.isSynchronous = true
+            
+            let retinaScale = UIScreen.main.scale;
+            let retinaSquare = CGSize(width: 100*retinaScale, height: 100*retinaScale)
+
+            let cropSideLength = min(asset.pixelWidth, asset.pixelHeight)
+            let x = asset.pixelWidth > asset.pixelHeight ? abs(asset.pixelWidth - asset.pixelHeight) / 2 : 0
+            let y = asset.pixelWidth > asset.pixelHeight ? 0 : abs(asset.pixelWidth - asset.pixelHeight) / 2
+            let square = CGRect(x: x, y: y, width: cropSideLength, height: cropSideLength)
+            let cropRect = square.applying(CGAffineTransform(scaleX: 1.0 / CGFloat(asset.pixelWidth), y: 1.0 / CGFloat(asset.pixelHeight)))
+            options.normalizedCropRect = cropRect
+            
+            PHImageManager.default().requestImage(for: asset, targetSize: retinaSquare, contentMode: .aspectFit, options: options, resultHandler: {(result, info)->Void in
+                DispatchQueue.main.async {
+                    imageView.image = result
+                }
+            })
+//        }
         
         return cell
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        if segue.identifier == "removeFlare" {
-            let cell = sender as! UICollectionViewCell
-            let indexPath = collectionView!.indexPath(for: cell)
-            let navigationController = segue.destination as! UINavigationController
-            let imageViewController = navigationController.viewControllers.first as! ImageViewController
-            imageViewController.asset = assets!.object(at: indexPath!.row)
+        if segue.identifier == "Photo" {
+            let cell = sender as! ViewCell
+            let imageViewController = segue.destination as! ImageViewController
+            imageViewController.asset = cell.asset
         }
     }
 
--- UnFlare/ImageViewController.swift
+++ UnFlare/ImageViewController.swift
@@ -13,61 +13,54 @@ class ImageViewController: UIViewController {
     
     var asset: PHAsset?
     @IBOutlet weak var imageView: UIImageView!
-    @IBOutlet weak var uflareButton: UIBarButtonItem!
-    @IBOutlet weak var saveButton: UIBarButtonItem!
+    @IBOutlet weak var topButton: UIBarButtonItem!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        PHImageManager.default().requestImage(for: asset!, targetSize: CGSize(width: asset!.pixelWidth, height: asset!.pixelHeight), contentMode: .aspectFit, options: nil, resultHandler: { result, info in
+        let options = PHImageRequestOptions()
+        options.isNetworkAccessAllowed = true
+        
+        PHImageManager.default().requestImage(for: asset!, targetSize: CGSize(width: asset!.pixelWidth, height: asset!.pixelHeight), contentMode: .aspectFit, options: options, resultHandler: { result, info in
             DispatchQueue.main.async {
                 self.imageView.image = result
             }
         })
-        
-        saveButton.isEnabled = false
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        
-        self.uflareButton.isEnabled = true
+    
+        topButton.title = "UnFlare"
+        topButton.isEnabled = true
     }
     
-    @IBAction func unflare(_ sender: AnyObject) {
-        uflareButton.isEnabled = false
-        let pending = UIAlertController(title: "Unflaring...", message: nil, preferredStyle: .alert)
-
-        present(pending, animated: true, completion: {
-            let image = processImage(self.imageView.image!)
-            DispatchQueue.main.async {
-                self.imageView.image = image
-                pending.dismiss(animated: true)
-                self.saveButton.isEnabled = true
-            }
-        })
-    }
-    
-    @IBAction func save(_ sender: AnyObject) {
-        saveButton.isEnabled = false
-        asset!.requestContentEditingInput(with: nil, completionHandler: { input, list in
-            let output = PHContentEditingOutput(contentEditingInput: input!)
-            output.adjustmentData = PHAdjustmentData(formatIdentifier: "io.nexan.apps.UnFlare", formatVersion: "1.0", data: "1.0".data(using: .utf8, allowLossyConversion: true)!)
-
-            try! UIImageJPEGRepresentation(self.imageView!.image!, 1)?.write(to: output.renderedContentURL)
-
-            PHPhotoLibrary.shared().performChanges({
-                let request = PHAssetChangeRequest(for: self.asset!)
-                request.contentEditingOutput = output
-            })
+    @IBAction func action(_ sender: AnyObject) {
+        if topButton.title == "UnFlare" {
+            let pending = UIAlertController(title: "Unflaring...", message: nil, preferredStyle: .alert)
             
-            DispatchQueue.main.async {
-                self.dismiss(animated: true)
-            }
-        })
-    }
-    
-    @IBAction func cancel(_ sender: AnyObject) {
-        dismiss(animated: true)
+            present(pending, animated: true, completion: {
+                let image = processImage(self.imageView.image!)
+                DispatchQueue.main.async {
+                    self.imageView.image = image
+                    pending.dismiss(animated: true)
+                    self.topButton.title = "Save"
+                }
+            })
+        }
+        else {
+            topButton.isEnabled = false
+            asset!.requestContentEditingInput(with: nil, completionHandler: { input, list in
+                let output = PHContentEditingOutput(contentEditingInput: input!)
+                output.adjustmentData = PHAdjustmentData(formatIdentifier: "io.nexan.apps.UnFlare", formatVersion: "1.0", data: "1.0".data(using: .utf8, allowLossyConversion: true)!)
+
+                try! UIImageJPEGRepresentation(self.imageView!.image!, 1)?.write(to: output.renderedContentURL)
+
+                PHPhotoLibrary.shared().performChanges({
+                    let request = PHAssetChangeRequest(for: self.asset!)
+                    request.contentEditingOutput = output
+                })
+            })
+        }
     }
 }
